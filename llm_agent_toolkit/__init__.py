6551174269e59ef5188bd7832ab8eb221522c6b4
JonahWhaler <jonah_mytzuchi@outlook.com>
2024-11-21 22:14:42 +0800

    Implement wrapper for local and remote encoder

--- llm_agent_toolkit/__init__.py
+++ llm_agent_toolkit/__init__.py
@@ -1,11 +1,12 @@
 from ._tool import Tool, FunctionInfoDict
 from ._util import ChatCompletionConfig, TranscriptionConfig
 from ._audio import AudioHelper
-from . import tool, loader
+from . import tool, loader, encoder
 
 __all__ = [
     "tool",
     "loader",
+    "encoder",
     "Tool",
     "FunctionInfoDict",
     "ChatCompletionConfig",
