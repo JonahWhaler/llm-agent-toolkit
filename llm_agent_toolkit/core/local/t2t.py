f4ae57926592b08084e14a9b0200da3bb376bde6
JonahWhaler <jonah_mytzuchi@outlook.com>
2024-11-26 12:04:07 +0800

    Add tool calling to Text-to-Text Core

--- llm_agent_toolkit/core/local/t2t.py
+++ llm_agent_toolkit/core/local/t2t.py
@@ -1,4 +1,5 @@
 # import os
+import json
 import warnings
 import logging
 import ollama
@@ -9,16 +10,34 @@ from ..._util import (
     MessageBlock,
 )
 
-# from ..._tool import Tool
+from ..._tool import Tool
 
 
 logger = logging.getLogger(__name__)
 
 
+TOOL_PROMPT = """
+Utilize tools to solve the problems. 
+Results from tools will be kept in the context. 
+Calling the tools repeatedly is highly discouraged.
+"""
+
+
 class T2T_OLM_Core(Core):
-    def __init__(self, system_prompt: str, config: ChatCompletionConfig):
+    def __init__(
+        self,
+        connection_string: str,
+        system_prompt: str,
+        config: ChatCompletionConfig,
+        tools: list[Tool] | None = None,
+    ):
         assert isinstance(config, ChatCompletionConfig)
-        super().__init__(system_prompt, config, None)
+        super().__init__(system_prompt, config, tools)
+        self.__connection_string = connection_string
+
+    @property
+    def CONN_STRING(self) -> str:
+        return self.__connection_string
 
     def run(
         self, query: str, context: list[MessageBlock | dict] | None, **kwargs
@@ -29,6 +48,16 @@ class T2T_OLM_Core(Core):
         if context is not None:
             msgs.extend(context)
         msgs.append(MessageBlock(role=CreatorRole.USER.value, content=query))
+        if self.tools is not None:
+            tools_metadata = []
+            for tool in self.tools:
+                tools_metadata.append(tool.info)
+            msgs.append(
+                MessageBlock(role=CreatorRole.SYSTEM.value, content=TOOL_PROMPT)
+            )
+            logger.info(TOOL_PROMPT)
+        else:
+            tools_metadata = None
         number_of_primers = len(msgs)
         if isinstance(self.config, ChatCompletionConfig):
             temperature = self.config.temperature
@@ -40,24 +69,35 @@ class T2T_OLM_Core(Core):
         token_count = 0
         solved = False
         try:
-            client = ollama.Client(host="http://localhost:11434")
+            client = ollama.Client(host=self.CONN_STRING)
             while iteration < self.config.max_iteration and token_count < max_tokens:
                 print(f"\n\nIteration: {iteration}")
                 response = client.chat(
                     model=self.model_name,
                     messages=msgs,
+                    tools=tools_metadata,
                     stream=False,
                     options={"temperature": temperature, "num_predict": max_tokens},
                 )
-                logger.info(response)
+
                 llm_generated_content = response["message"]["content"]
-                msgs.append(
-                    MessageBlock(
-                        role=CreatorRole.ASSISTANT.value, content=llm_generated_content
+                if llm_generated_content != "":
+                    msgs.append(
+                        MessageBlock(
+                            role=CreatorRole.ASSISTANT.value,
+                            content=llm_generated_content,
+                        )
                     )
-                )
+
+                tool_calls = response["message"]["tool_calls"]
+                if tool_calls is None:
+                    solved = True
+                    break
+
+                output = self.__call_tools(tool_calls)
+                msgs.extend(output)
+
                 iteration += 1
-                break
 
             if not solved:
                 if iteration == self.config.max_iteration:
@@ -70,7 +110,7 @@ class T2T_OLM_Core(Core):
                     )
             return msgs[number_of_primers:]  # Return only the generated messages
         except Exception as e:
-            logger.error(f"run: {e}")
+            logger.error("Error: %s", e)
             raise
 
     async def run_async(
@@ -82,6 +122,16 @@ class T2T_OLM_Core(Core):
         if context is not None:
             msgs.extend(context)
         msgs.append(MessageBlock(role=CreatorRole.USER.value, content=query))
+        if self.tools is not None:
+            tools_metadata = []
+            for tool in self.tools:
+                tools_metadata.append(tool.info)
+            msgs.append(
+                MessageBlock(role=CreatorRole.SYSTEM.value, content=TOOL_PROMPT)
+            )
+            logger.info(TOOL_PROMPT)
+        else:
+            tools_metadata = None
         number_of_primers = len(msgs)
         if isinstance(self.config, ChatCompletionConfig):
             temperature = self.config.temperature
@@ -93,24 +143,35 @@ class T2T_OLM_Core(Core):
         token_count = 0
         solved = False
         try:
-            client = ollama.AsyncClient(host="http://localhost:11434")
+            client = ollama.AsyncClient(host=self.CONN_STRING)
             while iteration < self.config.max_iteration and token_count < max_tokens:
                 print(f"\n\nIteration: {iteration}")
                 response = await client.chat(
                     model=self.model_name,
                     messages=msgs,
+                    tools=tools_metadata,
                     stream=False,
                     options={"temperature": temperature, "num_predict": max_tokens},
                 )
-                logger.info(response)
+
                 llm_generated_content = response["message"]["content"]
-                msgs.append(
-                    MessageBlock(
-                        role=CreatorRole.ASSISTANT.value, content=llm_generated_content
+                if llm_generated_content != "":
+                    msgs.append(
+                        MessageBlock(
+                            role=CreatorRole.ASSISTANT.value,
+                            content=llm_generated_content,
+                        )
                     )
-                )
+
+                tool_calls = response["message"]["tool_calls"]
+                if tool_calls is None:
+                    solved = True
+                    break
+
+                output = await self.__call_tools_async(tool_calls)
+                msgs.extend(output)
+
                 iteration += 1
-                break
 
             if not solved:
                 if iteration == self.config.max_iteration:
@@ -123,5 +184,63 @@ class T2T_OLM_Core(Core):
                     )
             return msgs[number_of_primers:]  # Return only the generated messages
         except Exception as e:
-            logger.error(f"run: {e}")
+            logger.error("Error: %s", e)
             raise
+
+    async def __call_tools_async(
+        self, selected_tools: list
+    ) -> list[MessageBlock | dict]:
+        output: list[MessageBlock | dict] = []
+
+        for tool_call in selected_tools:
+            for tool in self.tools:  # type: ignore
+                if tool.info["function"]["name"] != tool_call.function.name:
+                    continue
+                args = json.dumps(tool_call.function.arguments)
+                try:
+                    result = await tool.run_async(args)
+                    output.append(
+                        {
+                            "role": CreatorRole.TOOL.value,
+                            "name": tool_call.function.name,
+                            "content": f"({args}) => {result}",
+                        }
+                    )
+                except Exception as e:
+                    output.append(
+                        MessageBlock(
+                            role=CreatorRole.TOOL.value,
+                            content=f"({args}) => {e}",
+                            name=tool_call.function.name,
+                        )
+                    )
+
+        return output
+
+    def __call_tools(self, selected_tools: list) -> list[MessageBlock | dict]:
+        output: list[MessageBlock | dict] = []
+
+        for tool_call in selected_tools:
+            for tool in self.tools:  # type: ignore
+                if tool.info["function"]["name"] != tool_call.function.name:
+                    continue
+                args = json.dumps(tool_call.function.arguments)
+                try:
+                    result = tool.run(args)
+                    output.append(
+                        {
+                            "role": CreatorRole.TOOL.value,
+                            "name": tool_call.function.name,
+                            "content": f"({args}) => {result}",
+                        }
+                    )
+                except Exception as e:
+                    output.append(
+                        MessageBlock(
+                            role=CreatorRole.TOOL.value,
+                            content=f"({args}) => {e}",
+                            name=tool_call.function.name,
+                        )
+                    )
+
+        return output
