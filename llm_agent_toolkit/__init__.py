c09c61f208262a6c9e985789dd67141021ea7b84
JonahWhaler <jonah_mytzuchi@outlook.com>
2024-12-07 19:44:20 +0800

    Expose abstract base class

--- llm_agent_toolkit/__init__.py
+++ llm_agent_toolkit/__init__.py
@@ -9,6 +9,10 @@ from ._tool import (
 from ._util import ChatCompletionConfig, TranscriptionConfig, ImageGenerationConfig
 from ._audio import AudioHelper
 from ._chunkers import Chunker, ChunkerMetrics, RandomInitializer, UniformInitializer
+from ._core import Core
+from ._memory import VectorMemory
+from ._encoder import Encoder
+from ._loader import BaseLoader
 from . import core, tool, loader, encoder, memory, chunkers
 
 __all__ = [
@@ -32,4 +36,8 @@ __all__ = [
     "RandomInitializer",
     "UniformInitializer",
     "chunkers",
+    "Core",
+    "Encoder",
+    "VectorMemory",
+    "BaseLoader",
 ]
