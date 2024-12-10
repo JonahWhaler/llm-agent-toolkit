e3519afd9b6000a4e414d5bd5a72e41d36659f56
JonahWhaler <jonah_mytzuchi@outlook.com>
2024-12-10 17:51:49 +0800

    Implement method to buidl profile based on *.csv file

--- llm_agent_toolkit/core/local/t2t.py
+++ llm_agent_toolkit/core/local/t2t.py
@@ -86,6 +86,52 @@ class T2T_OLM_Core(Core):
         except Exception as e:
             logger.error("Exception: %s (%s)", str(e), type(e))
             raise
+
+    @staticmethod
+    def __build_profile(model_name: str) -> dict[str, bool | int | str]:
+        """
+        Build the profile dict based on information found in ./llm_agent_toolkit/core/local/ollama.csv
+
+        These are the models which the developer has experience with.
+        If `model_name` is not found in the csv file, default value will be applied.
+
+        Call .set_context_length to set the context length, default value is 2048.
+        """
+        logger.info("Building profile...")
+        profile: dict[str, bool | int | str] = {"name": model_name}
+        with open(
+            "./llm_agent_toolkit/core/local/ollama.csv", "r", encoding="utf-8"
+        ) as csv:
+            header = csv.readline()
+            columns = header.strip().split(",")
+            while True:
+                line = csv.readline()
+                if not line:
+                    break
+                values = line.strip().split(",")
+                if values[0] == model_name:
+                    for column, value in zip(columns[1:], values[1:]):
+                        if column == "context_length":
+                            profile[column] = int(value)
+                        elif column == "remarks":
+                            profile[column] = value
+                        elif value == "TRUE":
+                            profile[column] = True
+                        else:
+                            profile[column] = False
+                    break
+
+        if "context_length" not in profile:
+            # Most supported context length
+            profile["context_length"] = 2048
+        if "tool" not in profile:
+            # Assume supported
+            profile["tool"] = True
+        if "text_generation" not in profile:
+            # Assume supported
+            profile["text_generation"] = True
+        logger.info("Profile ready")
+        return profile
     @property
     def CONN_STRING(self) -> str:
         return self.__connection_string
