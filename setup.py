f34e801514fa7559c489427a5f7fcb8fd9d1c058
JonahWhaler <jonah_mytzuchi@outlook.com>
2025-04-15 15:24:51 +0800

    Use charade to detect non-ascii string.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ with open("./README.md", "r", encoding="utf-8") as f:
 
 core_dependencies = [
     "python-dotenv==0.21.0",
+    "charade==1.0.3",
     "openai==1.66.5",
     "tiktoken==0.9.0",
     "chromadb==0.5.11",
