27fbefe269f26a0e9a5e77df00bd7a18df68de74
JonahWhaler <jonah_mytzuchi@outlook.com>
2025-04-28 17:12:15 +0800

    Handle rate limit error.

--- llm_agent_toolkit/encoder/gemini.py
+++ llm_agent_toolkit/encoder/gemini.py
@@ -1,7 +1,8 @@
-from curses.ascii import isascii
 import os
 import logging
+import time
 import asyncio
+from typing import Optional
 from concurrent.futures import ThreadPoolExecutor
 from google import genai
 from google.genai import types
@@ -18,7 +19,9 @@ class GeminiEncoder(Encoder):
     """
 
     SUPPORTED_MODELS = (
-        EncoderProfile(name="models/embedding-001", dimension=768, ctx_length=2048),
+        EncoderProfile(
+            name="models/embedding-001", dimension=768, ctx_length=2048
+        ),
         EncoderProfile(
             name="models/text-embedding-004", dimension=768, ctx_length=2048
         ),
@@ -40,11 +43,14 @@ class GeminiEncoder(Encoder):
 
     def __init__(self, model_name: str, dimension: int, task_type: str):
         for profile in GeminiEncoder.SUPPORTED_MODELS:
-            if model_name == profile["name"] and dimension == profile["dimension"]:
+            if model_name == profile["name"] and dimension == profile[
+                "dimension"]:
                 ctx_length = profile["ctx_length"]
                 break
         else:
-            raise ValueError("Either model name or dimension are not supported.")
+            raise ValueError(
+                "Either model name or dimension are not supported."
+            )
         super().__init__(model_name, dimension, ctx_length)
 
         if task_type not in GeminiEncoder.SUPPORTED_TASK_TYPES:
@@ -76,8 +82,7 @@ class GeminiEncoder(Encoder):
                 config=self.config,
             )
             if (
-                response.embeddings
-                and response.embeddings[0]
+                response.embeddings and response.embeddings[0]
                 and response.embeddings[0].values
             ):
                 return response.embeddings[0].values
@@ -90,6 +95,25 @@ class GeminiEncoder(Encoder):
                 exc_info=True,
                 stack_info=True,
             )
+            if "502 Bad Gateway" in str(e):
+                delay: Optional[float] = kwargs.get("delay", None)
+                attempt: Optional[int] = kwargs.get("attempt", None)
+                if delay is None:
+                    delay = 5.0
+
+                if attempt is None:
+                    attempt = 1
+
+                if attempt > 5:
+                    logger.warning("Max attempts reached. Raising error.")
+                    raise
+
+                logger.warning(
+                    "[%d] 502 Bad Gateway. Retrying in %.2f seconds.", attempt,
+                    delay
+                )
+                time.sleep(delay)
+                return self.encode(text, delay=delay * 1.5, attempt=attempt + 1)
             raise
 
     @staticmethod
@@ -114,8 +138,7 @@ class GeminiEncoder(Encoder):
                 model_name=self.model_name, config=self.config, text=text
             )
             if (
-                response.embeddings
-                and response.embeddings[0]
+                response.embeddings and response.embeddings[0]
                 and response.embeddings[0].values
             ):
                 return response.embeddings[0].values
@@ -128,6 +151,28 @@ class GeminiEncoder(Encoder):
                 exc_info=True,
                 stack_info=True,
             )
+            if "502 Bad Gateway" in str(e):
+                delay: Optional[float] = kwargs.get("delay", None)
+                attempt: Optional[int] = kwargs.get("attempt", None)
+                if delay is None:
+                    delay = 5.0
+
+                if attempt is None:
+                    attempt = 1
+
+                if attempt > 5:
+                    logger.warning("Max attempts reached. Raising error.")
+                    raise
+
+                logger.warning(
+                    "[%d] 502 Bad Gateway. Retrying in %.2f seconds.", attempt,
+                    delay
+                )
+                await asyncio.sleep(delay)
+                return await self.encode_async(
+                    text, delay=delay * 1.5, attempt=attempt + 1
+                )
+
             raise
 
     def encode_v2(self, text: str, **kwargs) -> tuple[list[float], int]:
@@ -141,9 +186,12 @@ class GeminiEncoder(Encoder):
             )
             raise
 
-    async def encode_v2_async(self, text: str, **kwargs) -> tuple[list[float], int]:
+    async def encode_v2_async(self, text: str,
+                              **kwargs) -> tuple[list[float], int]:
         try:
-            return (await self.encode_async(text), self.estimate_token_count(text))
+            return (
+                await self.encode_async(text), self.estimate_token_count(text)
+            )
         except Exception as e:
             logger.error(
                 msg=f"{self.model_name}.encode failed. Error: {str(e)}",
--- llm_agent_toolkit/encoder/remote.py
+++ llm_agent_toolkit/encoder/remote.py
@@ -1,6 +1,10 @@
 import os
 import logging
+import time
+import asyncio
+from typing import Optional
 import openai
+from openai import RateLimitError
 from .._encoder import Encoder, EncoderProfile
 
 logger = logging.getLogger(name=__name__)
@@ -8,21 +12,36 @@ logger = logging.getLogger(name=__name__)
 
 class OpenAIEncoder(Encoder):
     SUPPORTED_MODELS = (
-        EncoderProfile(name="text-embedding-3-small", dimension=512, ctx_length=8191),
-        EncoderProfile(name="text-embedding-3-small", dimension=1536, ctx_length=8191),
-        EncoderProfile(name="text-embedding-3-large", dimension=256, ctx_length=8191),
-        EncoderProfile(name="text-embedding-3-large", dimension=512, ctx_length=8191),
-        EncoderProfile(name="text-embedding-3-large", dimension=1024, ctx_length=8191),
-        EncoderProfile(name="text-embedding-3-large", dimension=3072, ctx_length=8191),
+        EncoderProfile(
+            name="text-embedding-3-small", dimension=512, ctx_length=8191
+        ),
+        EncoderProfile(
+            name="text-embedding-3-small", dimension=1536, ctx_length=8191
+        ),
+        EncoderProfile(
+            name="text-embedding-3-large", dimension=256, ctx_length=8191
+        ),
+        EncoderProfile(
+            name="text-embedding-3-large", dimension=512, ctx_length=8191
+        ),
+        EncoderProfile(
+            name="text-embedding-3-large", dimension=1024, ctx_length=8191
+        ),
+        EncoderProfile(
+            name="text-embedding-3-large", dimension=3072, ctx_length=8191
+        ),
     )
 
     def __init__(self, model_name: str, dimension: int):
         for profile in OpenAIEncoder.SUPPORTED_MODELS:
-            if model_name == profile["name"] and dimension == profile["dimension"]:
+            if model_name == profile["name"] and dimension == profile[
+                "dimension"]:
                 ctx_length = profile["ctx_length"]
                 break
         else:
-            raise ValueError("Either model name or dimension are not supported.")
+            raise ValueError(
+                "Either model name or dimension are not supported."
+            )
         super().__init__(model_name, dimension, ctx_length)
 
     def encode(self, text: str, **kwargs) -> list[float]:
@@ -34,6 +53,30 @@ class OpenAIEncoder(Encoder):
                 input=text,
             )
             return response.data[0].embedding
+        except RateLimitError as rle:
+            logger.error(
+                msg=
+                f"{self.model_name}.encode failed. Rate limit error: {str(rle)}",
+                exc_info=True,
+                stack_info=True,
+            )
+
+            delay: Optional[float] = kwargs.get("delay", None)
+            attempt: Optional[int] = kwargs.get("attempt", None)
+
+            if delay is None:
+                delay = 5.0
+
+            if attempt is None:
+                attempt = 1
+
+            if attempt > 5:
+                logger.warning("Max attempts reached. Raising error.")
+                raise
+
+            logger.info(f"Retrying in {delay} seconds...")
+            time.sleep(delay)
+            return self.encode(text, delay=delay * 1.5, attempt=attempt + 1)
         except Exception as e:
             logger.error(
                 msg=f"{self.model_name}.encode failed. Error: {str(e)}",
@@ -51,6 +94,31 @@ class OpenAIEncoder(Encoder):
                 input=text,
             )
             return (response.data[0].embedding, response.usage.total_tokens)
+
+        except RateLimitError as rle:
+            logger.error(
+                msg=
+                f"{self.model_name}.encode failed. Rate limit error: {str(rle)}",
+                exc_info=True,
+                stack_info=True,
+            )
+
+            delay: Optional[float] = kwargs.get("delay", None)
+            attempt: Optional[int] = kwargs.get("attempt", None)
+
+            if delay is None:
+                delay = 5.0
+
+            if attempt is None:
+                attempt = 1
+
+            if attempt > 5:
+                logger.warning("Max attempts reached. Raising error.")
+                raise
+
+            logger.info(f"Retrying in {delay} seconds...")
+            time.sleep(delay)
+            return self.encode_v2(text, delay=delay * 1.5, attempt=attempt + 1)
         except Exception as e:
             logger.error(
                 msg=f"{self.model_name}.encode failed. Error: {str(e)}",
@@ -68,6 +136,33 @@ class OpenAIEncoder(Encoder):
                 input=text,
             )
             return response.data[0].embedding
+
+        except RateLimitError as rle:
+            logger.error(
+                msg=
+                f"{self.model_name}.encode failed. Rate limit error: {str(rle)}",
+                exc_info=True,
+                stack_info=True,
+            )
+
+            delay: Optional[float] = kwargs.get("delay", None)
+            attempt: Optional[int] = kwargs.get("attempt", None)
+
+            if delay is None:
+                delay = 5.0
+
+            if attempt is None:
+                attempt = 1
+
+            if attempt > 5:
+                logger.warning("Max attempts reached. Raising error.")
+                raise
+
+            logger.info(f"Retrying in {delay} seconds...")
+            await asyncio.sleep(delay)
+            return await self.encode_async(
+                text, delay=delay * 1.5, attempt=attempt + 1
+            )
         except Exception as e:
             logger.error(
                 msg=f"{self.model_name}.encode failed. Error: {str(e)}",
@@ -76,7 +171,8 @@ class OpenAIEncoder(Encoder):
             )
             raise
 
-    async def encode_v2_async(self, text: str, **kwargs) -> tuple[list[float], int]:
+    async def encode_v2_async(self, text: str,
+                              **kwargs) -> tuple[list[float], int]:
         try:
             client = openai.AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
             response = await client.embeddings.create(
@@ -85,6 +181,32 @@ class OpenAIEncoder(Encoder):
                 input=text,
             )
             return (response.data[0].embedding, response.usage.total_tokens)
+        except RateLimitError as rle:
+            logger.error(
+                msg=
+                f"{self.model_name}.encode failed. Rate limit error: {str(rle)}",
+                exc_info=True,
+                stack_info=True,
+            )
+
+            delay: Optional[float] = kwargs.get("delay", None)
+            attempt: Optional[int] = kwargs.get("attempt", None)
+
+            if delay is None:
+                delay = 5.0
+
+            if attempt is None:
+                attempt = 1
+
+            if attempt > 5:
+                logger.warning("Max attempts reached. Raising error.")
+                raise
+
+            logger.info(f"Retrying in {delay} seconds...")
+            await asyncio.sleep(delay)
+            return await self.encode_v2_async(
+                text, delay=delay * 1.5, attempt=attempt + 1
+            )
         except Exception as e:
             logger.error(
                 msg=f"{self.model_name}.encode failed. Error: {str(e)}",
